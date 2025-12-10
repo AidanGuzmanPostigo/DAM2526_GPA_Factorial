fb5256817a67676da4343d1ac31f93cd9f58f10f
aguzpos332 <aguzpos332@g.educaand.es>
2025-12-10 08:58:05 +0100

    D: Añadida verificación de tipo de dato en la función pedirNumero

--- src/pruebaControlVersiones/Factorial.java
+++ src/pruebaControlVersiones/Factorial.java
@@ -13,7 +13,20 @@ public class Factorial {
         System.out.println("RESULTADO - En versiones posteriores se calculará el factorial del número " + num);
 	}
 	private static int pedirNumero() {
-		System.out.print("Introduce un número entero mayor o igual que 0: ");
-		return sc.nextInt();
+		int num;
+	    boolean valido;
+	    do {
+	        System.out.print("Introduce un número entero mayor o igual que 0: ");
+	        if (sc.hasNextInt()) {
+	        	num = sc.nextInt();
+	        	valido = true;
+	        } else {
+	            System.out.println("Entrada no válida. Introduce un entero.");
+	            sc.next();
+	            valido = false;
+	            num = -1;
+	        }
+	    } while (!valido);
+		return num;
 	}
 }
